f6bfee09be007e87c99b4a23069ee50e66a95621
Muhammad Ali Hamza <aliansari2566@gmail.com>
2024-05-16 18:11:31 +0500

    Update index.jsx

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,35 +1,17 @@
-import React, { useState }from "react";
-import { Button, Form, Input, Col, Row, message } from "antd";
+import React from "react";
+import {Col, Row} from "antd";
 
 import { DynamicHeader } from "../../components/DynamicHeader";
 import Image from "../../assets/svg/screenImage.svg";
 
 
 const Login = () => {
-  const [form] = Form.useForm();
-
-  const [isButtonDisabled, setButtonDisabled] = useState(true); 
-  const onFinish = async (values) => {
-    try {
-      await form.validateFields();
-     
-      console.log(values);
-    } catch (error) {
-      console.error("Error submitting form:", error);
-      message.error("An error occurred while processing your request.");
-    }
-  };
-  const handleValuesChange = (changedValues, allValues) => {
-    // Check if any field is empty
-    const isEmptyField = Object.values(allValues).some((value) => value === undefined || value === '');
-    // Disable or enable button based on the presence of empty fields
-    setButtonDisabled(isEmptyField);
-  };
+  
   return (
     <>
       <div className="Login">
         <div className="content-container">
-          <DynamicHeader buttonText="Log in" />
+          <DynamicHeader buttonText="Sign in" />
           <Row justify="center" className="container-row">
             <Col className="loginLeft" xs={24} sm={24} md={12} lg={12} xl={12}>
               <div className="loginLeftcontent">
